c5e817e9f41143e5b35cb375127169ca394db56c
Lionel Chanson <github@lionelchanson.fr>
2017-07-10 11:37:45 +0200

    test: test get_index_list return a list of index for 2 words

--- tests.py
+++ tests.py
@@ -1,4 +1,23 @@
 import unittest
 
+def get_list_index(seq):
+    list_index = []
+    for i in range(len(seq)):
+        list_index.append(i)
+    return list_index
+
+class NommerTestCase(unittest.TestCase):
+    """
+    TestCase for Nommer
+    """
+
+    def test_list_of_index_with_2_words(self):
+        """
+        Test get_list_index return a list of index from given list of 2 words
+        """
+        words = ('hello', 'world')
+        expected_list = [0, 1]
+        self.assertEqual(expected_list, get_list_index(words))
+
 if __name__ == '__main__':
     unittest.main()

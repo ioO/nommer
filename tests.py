83804762574da738dc6c8101c3aaf40ca4d90df2
Lionel Chanson <github@lionelchanson.fr>
2017-07-10 12:27:47 +0200

    refactor: refactor list of words in tests

--- tests.py
+++ tests.py
@@ -16,41 +16,42 @@ class NommerTestCase(unittest.TestCase):
     TestCase for Nommer
     """
 
+    def setUp(self):
+        self.two_words = ('hello', 'world')
+        self.three_words = ('hello', 'world', 'how')
+        self.five_words = ('hello', 'world', 'how', 'are', 'you')
+        self.n_words = list('helloworldhowareyou')
+
     def test_list_of_index_with_2_words(self):
         """
         Test get_list_index return a list of index from given list of 2 words
         """
-        words = ('hello', 'world')
         expected_list = [0, 1]
-        self.assertEqual(expected_list, get_list_index(words))
+        self.assertEqual(expected_list, get_list_index(self.two_words))
 
     def test_list_of_index_with_3_words(self):
         """
         Test get_list_index return a list of index from given list of 3 words
         """
-        words = ('hello', 'world', 'how')
         expected_list = [0, 1, 2]
-        self.assertEqual(expected_list, get_list_index(words))
+        self.assertEqual(expected_list, get_list_index(self.three_words))
 
     def test_list_of_index_with_5_words(self):
         """
         Test get_list_index return a list of index from given list of 5 words
         """
-        words = ('hello', 'world', 'how', 'are', 'you')
         expected_list = [0, 1, 2, 3, 4]
-        self.assertEqual(expected_list, get_list_index(words))
+        self.assertEqual(expected_list, get_list_index(self.five_words))
 
     def test_list_of_index_with_n_words(self):
         """
         Test get_list_index return a list of index from given list of n words
         Use a random function for each test
         """
-        #split each letter
-        words = ('helloworldhowareyou')
         #random number for list
-        rnum = random.randint(0,len(words))
+        rnum = random.randint(0,len(self.n_words))
         #split the list
-        new_list = words[0:rnum]
+        new_list = self.n_words[0:rnum]
         expected_list = list(range(rnum))
         self.assertEqual(expected_list, get_list_index(new_list))
 
@@ -58,8 +59,7 @@ class NommerTestCase(unittest.TestCase):
         """
         Test find index range return an integer value
         """
-        words = ('hello', 'world')
-        list_index = get_list_index(words)
+        list_index = get_list_index(self.two_words)
         self.assertEqual(find_index_range(list_index), 10)
 
 if __name__ == '__main__':

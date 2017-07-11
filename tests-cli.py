f114f98ffc606bb2358b3b1a71ccbd922b42e58a
Lionel Chanson <github@lionelchanson.fr>
2017-07-11 16:16:14 +0200

    test: base class for tests

---
+++ tests-cli.py
@@ -0,0 +1,11 @@
+import unittest
+
+class NommerCliTestCase(unittest.TestCase):
+    """
+    Test class for cli interface
+    """
+    def test_success(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()

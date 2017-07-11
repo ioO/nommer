73921effdc07f4e99b5366fc150300fe8d501562
Lionel Chanson <github@lionelchanson.fr>
2017-07-12 00:01:52 +0200

    refactor(cli): remove call to removed method

--- nommer_cli.py
+++ nommer_cli.py
@@ -11,15 +11,7 @@ def main():
 
 def process(words):
     word_list = words.split(',')
-    list_index = get_list_index(word_list)
-    index_range = find_index_range(list_index)
-    list_integer = create_list_integer_range(index_range, len(list_index))
-    index_string = create_possible_index_string(list_index, list_integer)
-    possible_index = create_possible_index(index_string)
-    del(index_string)
-    combinations = create_combination(word_list, possible_index)
-    del(possible_index)
-    return create_name(combinations)
+    return get_combination_of_names(words.split(','))
 
 def display(names):
     for name in names:

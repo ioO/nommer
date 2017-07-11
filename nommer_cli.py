341f2db3ea27f0c2b756ce5bf84912e13c1ec189
Lionel Chanson <github@lionelchanson.fr>
2017-07-11 20:42:58 +0200

    refactor(nommer): delete variables after use to free memory

--- nommer_cli.py
+++ nommer_cli.py
@@ -16,7 +16,9 @@ def process(words):
     list_integer = create_list_integer_range(index_range, len(list_index))
     index_string = create_possible_index_string(list_index, list_integer)
     possible_index = create_possible_index(index_string)
+    del(index_string)
     combinations = create_combination(word_list, possible_index)
+    del(possible_index)
     return create_name(combinations)
 
 def display(names):
